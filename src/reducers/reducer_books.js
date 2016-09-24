1963881e4d5e0d9c2bd0c1877ebed3b0c2e716d5
Jamaica <jamaica@git.corp.sf>
2016-09-24 15:28:20 +0300

    Chapter 4 finished.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Javascritp: Harry Potter', pages: 101 },
+        { title: 'Goold Food: Potter', pages: 39 },
+        { title: 'Java: Dard Potter', pages: 14 },
+        { title: 'JFedde: Harry ', pages: 1 }
+    ]
+}
